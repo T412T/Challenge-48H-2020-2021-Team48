9f9cc919c0d18e1746ba9c79cf3aa32b2f0d54f5
Nyft <alexandre.kramer@ynov.com>
2021-02-22 15:41:57 +0100

    update 2

---
+++ public/tag.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+    </head>
+    <body>
+      <button type="button" name="button">tag viande</button>
+      <?php
+      $pdo5 = new PDO("mysql:host=localhost;dbname=data_image", "root", "" , array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+      $result = $pdo5->query("SELECT * FROM image  WHERE tag = '$_GET[tag]';");
+      while($info = $result->fetch(PDO::FETCH_OBJ)) {?>
+      <img src="<?php echo $info->link;?>" alt="img">
+      <?php }?>
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<?php
+$pdo5 = new PDO("mysql:host=localhost;dbname=data_image", "root", "" , array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+$result = $pdo5->query("SELECT * FROM image;");
+$result2 = $pdo5->query("SELECT * FROM image;");
+$info = $result->fetch(PDO::FETCH_OBJ);?>
     <head>
+
     </head>
     <body>
-      <button type="button" name="button">tag viande</button>
-      <?php
-      $pdo5 = new PDO("mysql:host=localhost;dbname=data_image", "root", "" , array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
-      $result = $pdo5->query("SELECT * FROM image;");
-      while($info = $result->fetch(PDO::FETCH_OBJ)) {?>
-      <img src="<?php echo $info->link;?>" alt="img">
+      <a href="tag.php?tag=<?php echo $info->tag ?>"><button type="button" name="button">tag viande</button></a>
+
+
+      <?php while($info2 = $result2->fetch(PDO::FETCH_OBJ)) {?>
+      <img src="<?php echo $info2->link;?>" alt="img">
       <?php }?>
     </body>
 </html>
